dbfa8448984ead1c84ab3fd77cd7269042f199d7
speed2CZ <odstrcilpetr@seznam.cz>
2025-10-14 20:41:49 +0200

    Changelog and bump version

--- mod_info.lua
+++ mod_info.lua
@@ -3,13 +3,13 @@
 -- Documentation for the extended FAF mod_info.lua format can be found here:
 -- https://github.com/FAForever/fa/wiki/mod_info.lua-documentation
 name = "FAF co-op support mod"
-version = 64
+version = 66
 _faf_modname = 'coop'
 copyright = "Forged Alliance Forever Community"
 description = "Support mod for coop maps"
 author = "Forged Alliance Forever Community"
 url = "http://www.faforever.com"
-uid = "35ddb804-d5b6-40db-9c2f-550066238a87"
+uid = "82fc9c3b-30bc-4c1a-9973-c5ab313081a2"
 selectable = false
 exclusive = false
 ui_only = false
